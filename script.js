3b1f82094f5cda06f4cc1c4641b67fdb639b8b61
Agnieszka <agnieszka.sekular@gmail.com>
2019-04-28 10:34:02 +0200

    timer new

--- script.js
+++ script.js
@@ -1,87 +1,84 @@
 //ZMIENNE
-let cards = document.getElementsByClassName("memory-card");
+let cards = document.getElementsByClassName('memory-card');
 cards = [...cards];
 const startTime = new Date().getTime();
-let flippedCard = "";
+let flippedCard = '';
 const flippedCards = [];
 const pairs = cards.length / 2;
 let result = 0;
 
 // BUTTON NOWA GRA
-const restart = function () {
-    location.reload()
+const restart = function() {
+  location.reload();
 };
-const go = function () {
-    shuffleCards();
-    setTimeout(function () {
-        cards.forEach(element => {
-            element.classList.add("back");
-            element.addEventListener("click", flipCard);
-        });
-    }, 2000)
-};
-const flipCard = function () {
-    flippedCard = this;
-    if(this === flippedCards[0]) return;
-    this.classList.remove("back");
-
-    if (flippedCards.length === 0) {
-        flippedCards[0] = this;
-        return;
-    } else {
-        cards.forEach(element => {
-            element.removeEventListener("click", flipCard)
-        });
-        flippedCards[1] = flippedCard;
-        console.log(flippedCards);
-
-        setTimeout(function(){
-            const theSame = flippedCards[0].dataset.pair === flippedCards[1].dataset.pair;
-            if(theSame) {
-                flippedCards.forEach(element =>
-                    element.classList.add("match"));
-                result++;
-                showResult();
-
-                cards = cards.filter(element =>
-                    !element.classList.contains("match"));
-
-                if (result === pairs) {
-                    const endTime = new Date().getTime();
-                    const gameTime = Math.floor((endTime - startTime) / 1000);
-                    alert (`WYGRANA GRA TWOJ WYNIK TO: ${gameTime} sekund`);
-                    restart()
-                }
-            } else {
-                flippedCards.forEach(element =>
-                    element.classList.add("back"));
-            }
-            flippedCards.length = 0;
-            flippedCard = "";
-            cards.forEach(element =>
-                element.addEventListener("click", flipCard));
-        },1000)
-    }
-};
-
-function shuffleCards () {
-    cards.forEach(card => {
-        card.style.order = Math.floor(Math.random() * cards.length);
+const go = function() {
+  shuffleCards();
+  setTimeout(function() {
+    cards.forEach((element) => {
+      element.classList.add('back');
+      element.addEventListener('click', flipCard);
     });
+  }, 2000);
+};
+const flipCard = function() {
+  flippedCard = this;
+  if (this === flippedCards[0]) return;
+  this.classList.remove('back');
+
+  if (flippedCards.length === 0) {
+    flippedCards[0] = this;
+    return;
+  } else {
+    cards.forEach((element) => {
+      element.removeEventListener('click', flipCard);
+    });
+    flippedCards[1] = flippedCard;
+    console.log(flippedCards);
+
+    setTimeout(function() {
+      const theSame =
+        flippedCards[0].dataset.pair === flippedCards[1].dataset.pair;
+      if (theSame) {
+        flippedCards.forEach((element) => element.classList.add('match'));
+        result++;
+        showResult();
+
+        cards = cards.filter((element) => !element.classList.contains('match'));
+
+        if (result === pairs) {
+          const endTime = new Date().getTime();
+          const gameTime = Math.floor((endTime - startTime) / 1000);
+          alert(`WYGRANA GRA TWOJ WYNIK TO: ${gameTime} sekund`);
+          restart();
+        }
+      } else {
+        flippedCards.forEach((element) => element.classList.add('back'));
+      }
+      flippedCards.length = 0;
+      flippedCard = '';
+      cards.forEach((element) => element.addEventListener('click', flipCard));
+    }, 1000);
+  }
+};
+
+function shuffleCards() {
+  cards.forEach((card) => {
+    card.style.order = Math.floor(Math.random() * cards.length);
+  });
 }
 go();
 
 // SHOW RESULT
-function showResult(){
-    let score = document.getElementById("score");
+function showResult() {
+  let score = document.getElementById('score');
 
-    if ((pairs-result)===1){
-        return score.innerHTML="Znajdź jeszcze " + (pairs - result) + " parę";
-    } else if ((pairs - result)>1&&(pairs-result)<5) {
-        score.innerHTML="Znajdź jeszcze " + (pairs-result) + " pary";
-    } else {
-        score.innerHTML="Znajdź jeszcze " + (pairs-result) + " par";
-    }
+  if (pairs - result === 1) {
+    return (score.innerHTML = 'Znajdź jeszcze ' + (pairs - result) + ' parę');
+  } else if (pairs - result > 1 && pairs - result < 5) {
+    score.innerHTML = 'Znajdź jeszcze ' + (pairs - result) + ' pary';
+  } else {
+    score.innerHTML = 'Znajdź jeszcze ' + (pairs - result) + ' par';
+  }
 }
 showResult();
 
@@ -89,24 +86,23 @@ showResult();
 let seconds = 0;
 let minutes = 0;
 
-setInterval(function(){
-    console.log(seconds);
-    seconds++;
-    if (seconds >= 60) {
-        minutes++;
-        seconds = 0;
-    }
+setInterval(function() {
+  console.log(seconds);
+  seconds++;
+  if (seconds >= 60) {
+    minutes++;
+    seconds = 0;
+  }
 
-    let timer = document.getElementById("timer");
-    if (seconds < 10) {
-        return  timer.innerHTML="CZAS: " + minutes + ":" + "0" + seconds;
-    } else {
-        return  timer.innerHTML="CZAS: " + minutes + ":" + seconds;
-    }
+  let timer = document.getElementById('timer');
 
-    if (minutes < 10) {
-        return  timer.innerHTML="CZAS: " + "0" + minutes + ":" + seconds;
-    } else{
-        return  timer.innerHTML="CZAS: " + minutes + ":" + seconds;
-    }
+  if (seconds < 10 && minutes < 10) {
+    timer.innerHTML = 'CZAS: ' + '0' + minutes + ':' + '0' + seconds;
+  } else if (seconds < 10 && minutes > 10) {
+    timer.innerHTML = 'CZAS: ' + minutes + ':' + '0' + seconds;
+  } else if (seconds > 10 && minutes > 10) {
+    timer.innerHTML = 'CZAS: ' + minutes + ':' + seconds;
+  } else {
+    timer.innerHTML = 'CZAS: ' + '0' + minutes + ':' + seconds;
+  }
 }, 1000);
